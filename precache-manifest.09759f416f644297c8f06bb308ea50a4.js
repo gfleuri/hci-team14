091337a90ad96a032e052bed38ee0aff50591ba3
gfleuri <gedeonfleurissaint@gmail.com>
2020-10-03 21:09:47 -0400

    Updates

---
+++ precache-manifest.09759f416f644297c8f06bb308ea50a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9179417adead559b68b871ff9e7b8e67",
+    "url": "/hci-team14/index.html"
+  },
+  {
+    "revision": "392f4e08b3243129e140",
+    "url": "/hci-team14/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c16c319d14e896e32a87",
+    "url": "/hci-team14/static/js/2.5a9501eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hci-team14/static/js/2.5a9501eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "392f4e08b3243129e140",
+    "url": "/hci-team14/static/js/main.5ef54fa6.chunk.js"
+  },
+  {
+    "revision": "96477b269c109ea79a01",
+    "url": "/hci-team14/static/js/runtime-main.a5876b39.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hci-team14/static/media/logo.5d5d9eef.svg"
+  }
+]);
