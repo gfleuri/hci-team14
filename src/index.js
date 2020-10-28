f05d7371704ab076cec9838e10ff5078c27f832d
gfleuri <gedeonfleurissaint@gmail.com>
2020-10-27 21:20:08 -0400

    removing hello img/section

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./App.js";
 import { CreateNote } from "./notes.js";
 import { LoadNotes } from "./notes.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
 ReactDOM.render(<LoadNotes />, document.getElementById("load-notes"));
 ReactDOM.render(<CreateNote />, document.getElementById("create-note"));
