2a32452bb72e795c8a21806318bc38956dcc2cb9
gfleuri <gedeonfleurissaint@gmail.com>
2020-10-28 19:16:44 -0400

    basic review functions

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { CreateNote } from "./notes.js";
 import { LoadNotes } from "./notes.js";
+import { ReviewNotes } from "./notes.js";
 
 ReactDOM.render(<LoadNotes />, document.getElementById("load-notes"));
 ReactDOM.render(<CreateNote />, document.getElementById("create-note"));
+ReactDOM.render(<ReviewNotes />, document.getElementById("review-notes"));
--- src/notes.js
+++ src/notes.js
@@ -72,6 +72,7 @@ function addNote() {
   );
   localStorage.setItem("note-id-" + newNoteID + "-visibility", "true");
   localStorage.setItem("note-id-" + newNoteID + "-difficulty", "Medium");
+  localStorage.setItem("note-id-" + newNoteID + "-review", "");
 
   // Increasing note counter by 1, locStor returns string so must parse
   localStorage.setItem(
@@ -100,6 +101,7 @@ function loadNotes() {
     let title = localStorage.getItem("note-id-" + i + "-title");
     let context = localStorage.getItem("note-id-" + i + "-context");
     let difficulty = localStorage.getItem("note-id-" + i + "-difficulty");
+    let review = localStorage.getItem("note-id-" + i + "-review");
     let search =
       "https://www.youtube.com/results?search_query=" + title.replace(" ", "+");
 
@@ -113,6 +115,12 @@ function loadNotes() {
         <div>
           <b>Difficulty: {difficulty}</b>
         </div>
+        {review !== "" && (
+          <div>
+            <br />
+            <b>Review by {review}</b>
+          </div>
+        )}
         <br />
         <div>
           <b>Title</b>
@@ -152,6 +160,87 @@ function loadNotes() {
     }
   }
   return loadedNotes;
+}
+
+/**
+ * Changes review setting
+ * @param {integer} days id of the note to be changed
+ **/
+function setReview(days) {
+  let date = new Date();
+  let date2 = new Date();
+  let separator = 0;
+  let perDay = parseInt(localStorage.getItem("note-count") / days);
+  if (localStorage.getItem("note-count") / days > perDay) {
+    perDay++;
+  }
+
+  // Resetting sort
+  localStorage.setItem("note-sort", "none");
+  // Getting specific note ID values
+  for (let i = localStorage.getItem("note-count") - 1; i >= 0; i--) {
+    date2.setDate(date.getDate() + separator);
+    localStorage.setItem("note-id-" + i + "-review", date2.toDateString());
+    if (i % perDay === 0) {
+      separator++;
+    }
+  }
+
+  // Refreshing notes
+  ReactDOM.render(<LoadNotes />, document.getElementById("load-notes"));
+  // Refreshing CreateNote - Updates newNoteID Value
+  ReactDOM.render(<CreateNote />, document.getElementById("create-note"));
+}
+
+/**
+ * Changes review setting
+ **/
+function removeReview() {
+  // Getting specific note ID values
+  for (let i = localStorage.getItem("note-count") - 1; i >= 0; i--) {
+    localStorage.setItem("note-id-" + i + "-review", "");
+  }
+
+  // Refreshing notes
+  ReactDOM.render(<LoadNotes />, document.getElementById("load-notes"));
+  // Refreshing CreateNote - Updates newNoteID Value
+  ReactDOM.render(<CreateNote />, document.getElementById("create-note"));
+}
+
+export class ReviewNotes extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { response: "" };
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div>
+        <div style={{ marginBottom: "10px" }}>Review Your Notes</div>
+        <div>How many days do you want for review?</div>
+        <button onClick={() => setReview(2)}>2</button>
+        <button onClick={() => setReview(3)}>3</button>
+        <button onClick={() => setReview(4)}>4</button>
+        <button onClick={() => setReview(5)}>5</button>
+        <button onClick={() => setReview(6)}>6</button>
+        <button onClick={() => setReview(7)}>7</button>
+        <button onClick={() => setReview(8)}>8</button>
+        <button onClick={() => setReview(9)}>9</button>
+        <button onClick={() => setReview(10)}>10</button>
+        <button onClick={() => setReview(11)}>11</button>
+        <button onClick={() => setReview(12)}>12</button>
+        <button onClick={() => setReview(13)}>13</button>
+        <button onClick={() => setReview(14)}>14</button>
+        <div>
+          <button onClick={() => removeReview()}>
+            Dsiabled Review Feature
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export class CreateNote extends React.Component {
