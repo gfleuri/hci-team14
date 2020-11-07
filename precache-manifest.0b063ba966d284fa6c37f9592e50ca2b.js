35abfc847c3ddc1d35c899e2de7ebdc0f7e14510
gfleuri <gedeonfleurissaint@gmail.com>
2020-11-07 13:20:59 -0500

    Updates

--- precache-manifest.0b063ba966d284fa6c37f9592e50ca2b.js
+++ precache-manifest.0b063ba966d284fa6c37f9592e50ca2b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4f11c9a291c0806cf8d3bac105872de3",
+    "revision": "23f58571dc84dd5249993f0724ed96fe",
     "url": "/hci-team14/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/hci-team14/static/js/2.a670165e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ea4ecddc607ef715840f",
-    "url": "/hci-team14/static/js/main.2d295cb3.chunk.js"
+    "revision": "c2b3395a5938d283a279",
+    "url": "/hci-team14/static/js/main.c4960e01.chunk.js"
   },
   {
     "revision": "96477b269c109ea79a01",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hci-team14/precache-manifest.cc6102baecb06b123ab9c66a6a62b2f9.js"
+  "/hci-team14/precache-manifest.0b063ba966d284fa6c37f9592e50ca2b.js"
 );
 
 self.addEventListener('message', (event) => {
